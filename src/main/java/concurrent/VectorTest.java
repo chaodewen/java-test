77f58497cd6978d5cd2c68e687c28ba5e0587312
Cc <vaal.just@qq.com>
2016-11-09 23:36:39 +0800

    edited VectorTest

--- src/main/java/concurrent/VectorTest.java
+++ src/main/java/concurrent/VectorTest.java
@@ -9,42 +9,39 @@ import java.util.Vector;
 /**
  * 深入理解Java虚拟机代码清单13-2
  *
- * 本义是说Vector是线程安全容器
+ * 意图是说明Vector是线程安全容器
  * 但是因为只是给add()、get()、size()等方法加synchronized关键字
  * 所以这段代码还是会抛出数组越界访问的异常
  * 需要在后面两个for循环外再加synchronized关键字
  *
- * 可能升级后的JDK做了修改
- * 目前代码能正常运行
+ * 原因是for循环中调用size()取到vector长度后
+ * 另一个线程可能调用remove()导致要get()的i失效
+ *
+ * 出错需要等待一段时间
  */
 public class VectorTest {
     private static Vector<Integer> vector = new Vector<>();
     public static void main(String[] args) {
         while(true) {
-            for(int i = 0; i < 10; i ++)
+            for (int i = 0; i < 10; i++)
                 vector.add(i);
 
-            Thread removeThread = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    for(int i = 0; i < vector.size(); i ++)
-                        vector.remove(i);
-                }
+            Thread removeThread = new Thread(() -> {
+                for (int i = 0; i < vector.size(); i++)
+                    vector.remove(i);
             });
 
-            Thread printThread = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    for(int i = 0; i < vector.size(); i ++)
-                        System.out.println(vector.get(i));
-                }
+            Thread printThread = new Thread(() -> {
+                for (int i = 0; i < vector.size(); i++)
+//                    System.out.println(vector.get(i));
+                    vector.get(i);
             });
 
             removeThread.start();
             printThread.start();
 
             // 不要同时产生过多线程，否则导致系统假死
-            while (Thread.activeCount() > 20);
+            while (Thread.activeCount() > 20) ;
         }
     }
 }
