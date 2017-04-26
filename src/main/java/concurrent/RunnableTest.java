abb8598e9475bf9fb977e2b54f4eb980811c0b6f
Cc <vaal.just@qq.com>
2017-04-27 00:03:34 +0800

    modify runnable test

--- src/main/java/concurrent/RunnableTest.java
+++ src/main/java/concurrent/RunnableTest.java
@@ -7,9 +7,11 @@ public class RunnableTest implements Runnable {
 	}
 	@Override
 	public void run() {
+		int num = 0;
 		for(int i = 0; i < 1000; i ++) {
-			System.out.println(text + ":" + i);
+//			System.out.println(text + ":" + i);
 		}
+		System.out.println("num : " + ++ num);
 	}
 	public static void main(String[] args) {
 //		RunnableTest rt1 = new RunnableTest("A");
@@ -18,8 +20,11 @@ public class RunnableTest implements Runnable {
 //		Thread t2 = new Thread(rt2);
 //		t1.start();
 //		t2.start();
-		for(int i = 0; i < 3; i ++) {
-			new Thread(new RunnableTest((char)('A' + i))).start();
-		}
+//		for(int i = 0; i < 3; i ++) {
+//			new Thread(new RunnableTest((char)('A' + i))).start();
+//		}
+		RunnableTest rt = new RunnableTest('t');
+		for(int i = 0; i < 3; i ++)
+			new Thread(rt).start();
 	}
 }
