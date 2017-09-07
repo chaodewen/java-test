c4e31294898e79d4d4f198f3a61841eb7cd05023
Cc <vaal.just@qq.com>
2017-09-08 00:13:40 +0800

    add comments on Sunday

--- src/main/java/algorithm/string/Sunday.java
+++ src/main/java/algorithm/string/Sunday.java
@@ -6,7 +6,7 @@ package algorithm.string;
 
 /**
  * i指txt,j指pat
- * 匹配失败时考虑i+pat.length()字符在pat中有没有出现
+ * 匹配失败时考虑i+patLength字符在pat中有没有出现
  * 没出现考虑再下一个字符
  * 出现则找到最后一个出现位置,然后对齐这个位置到该字符,尝试匹配
  */
@@ -14,23 +14,24 @@ public class Sunday {
     public static int sundaySearch(String txt, String pat) {
         int txtLength = txt.length(), patLength = pat.length();
 
-        for(int i = 0, j, skip = -1; i < txtLength - patLength + 1; i += skip) {
-            for(j = 0; j < patLength; j ++)
-                if(txt.charAt(i + j) != pat.charAt(j)) {
-                    if(i + patLength < txtLength) // 判断有没有需要判断的下一个合法字符
-                        skip = patLength - getIndex(pat, txt.charAt(i + patLength));
+        for (int i = 0, j, skip = -1; i < txtLength - patLength + 1; i += skip) {
+            for (j = 0; j < patLength; j++)
+                if (txt.charAt(i + j) != pat.charAt(j)) { // 找到不匹配的字母 向后跳
+                    skip = patLength - getIndex(pat, txt.charAt(i + patLength)); // 根据i+patLength的字母进行匹配
                     break;
                 }
 
-            if(j == patLength)
+            if (j == patLength) // 遍历未break 说明正常匹配
                 return i;
         }
 
         return -1;
     }
+
+    // 找到pat中最后面的c
     private static int getIndex(String pat, char c) {
-        for(int i = pat.length() - 1; i >= 0; i --) // 对pat从后往前匹配
-            if(pat.charAt(i) == c)
+        for (int i = pat.length() - 1; i >= 0; i--) // 对pat从后往前匹配
+            if (pat.charAt(i) == c)
                 return i;
 
         return -1;
